f5fb0d4eaaf0a4895a6503783455a25e46c64cf7
kyhosting <kikimodesad8@gmail.com>
2025-09-29 09:39:46 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Bot Configuration
+API_ID = 24556439  # Ganti dengan API ID Anda
+API_HASH = "6e397e76ab9b87a3a81813c3e053a144"  # Ganti dengan API Hash Anda
+BOT_TOKEN = "8476769994:AAG92eoGDyTF_Pw4ri9XgiYrYhPbO2hyCkY"  # Ganti dengan Bot Token Anda
+OWNER_ID = [8337475905]  # Ganti dengan ID Owner Anda
